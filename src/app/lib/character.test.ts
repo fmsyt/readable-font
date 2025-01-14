a231c2f7a47f9afa4719423c66c812c50278ac9d
motsuni <motsuni@yotta-lab.com>
2025-01-14 22:20:21 +0900

    Characterテストを追加し、createInputPatterns関数の動作を検証

--- src/app/lib/character.test.ts
+++ src/app/lib/character.test.ts
@@ -1,5 +1,32 @@
-describe("Character", () => {
-  it("sample", () => {
-    expect(1).toBe(1);
+import { createInputPatterns } from "./character";
+
+describe("Text", () => {
+  it("ちゃんと", () => {
+    const patternsList = createInputPatterns("ちゃんと");
+    const patternsStringList = patternsList.map((patterns) => patterns.join("")).sort();
+
+    expect(patternsStringList).toEqual([
+      "chanto",
+      "channto",
+      "chaxnto",
+      "tyanto",
+      "tyannto",
+      "tyaxnto",
+      "cyanto",
+      "cyannto",
+      "cyaxnto",
+      "tilyanto",
+      "tilyannto",
+      "tilyaxnto",
+      "chilyanto",
+      "chilyannto",
+      "chilyaxnto",
+      "tixyanto",
+      "tixyannto",
+      "tixyaxnto",
+      "chixyanto",
+      "chixyannto",
+      "chixyaxnto",
+    ].sort());
   });
 });
